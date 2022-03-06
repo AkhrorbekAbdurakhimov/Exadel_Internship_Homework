875c16144e3b52d6b39570bdf05982a899aa95f6
AkhrorbekAbdurakhimov <akhrorbekabdurakhimov@gmail.com>
2022-03-07 00:50:32 +0500

    adding eslint to project

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 
-const NODE_PORT = 5000;
-const NODE_DEV = 'development';
+const NODE_PORT = process.env.NODE_PORT || 5000;
+const NODE_DEV = process.env.NODE_DEV || 'development';
 
-const booksRouter = require("./routes/books");
+const booksRouter = require('./routes/books');
 
 app.use(express.json());
 app.use('/api/books', booksRouter);
 
-app.listen(NODE_PORT, () => { console.log(`${NODE_DEV} server is running on port ${NODE_PORT}`) });
+app.listen(NODE_PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`${NODE_DEV} server is running on port ${NODE_PORT}`);
+});
